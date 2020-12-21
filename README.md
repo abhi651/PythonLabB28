d6cb7fa2f1952b1415fbd0f15718bb54e8fcc733
abhi-651 <akumar2@gitam.edu>
2020-12-21 10:06:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# PythonLabB28" 
---
+++ bubble_sort.py
@@ -0,0 +1,37 @@
+""" write a program for Bubble Sort""" 
+
+import unittest
+
+# Implement the below function and run this file
+# Return the output, No need read input or print the ouput
+
+def bubble_sort(lst):
+	pass
+
+	        
+# DO NOT TOUCH THE BELOW CODE
+class TestBubbleSort(unittest.TestCase):
+    
+    def test_01(self):
+        lst = [1,4,3,2]
+        new_lst = [1,2,3,4]
+        self.assertEqual(bubble_sort(lst),new_lst)
+
+    def test_02(self):
+        lst = [0,0,0,0]
+        new_lst = [0,0,0,0]
+        self.assertEqual(bubble_sort(lst),new_lst)
+
+    def test_03(self):
+        lst = [-1,4,0,2]
+        new_lst = [-1,0,2,4]
+        self.assertEqual(bubble_sort(lst),new_lst)
+
+    def test_04(self):
+        lst = [11,101,1, 110111,1101]
+        new_lst = [1,11,101,1101,110111]
+        self.assertEqual(bubble_sort(lst),new_lst)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
